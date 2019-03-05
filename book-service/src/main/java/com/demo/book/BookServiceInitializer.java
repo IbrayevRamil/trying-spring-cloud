ead4852d51f9282b78652fc32e8cbf756f64364e
Ramil Ibraev <ramil_ibraev@epam.com>
2019-03-05 14:33:23 +0300

    fix

--- book-service/src/main/java/com/demo/book/BookServiceInitializer.java
+++ book-service/src/main/java/com/demo/book/BookServiceInitializer.java
@@ -1,9 +1,13 @@
 package com.demo.book;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class BookServiceInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceInitializer.class, args);
+    }
 }
