600e1becb001a29f9e7a38fe70ef1f2ce4bf8a14
Ramil Ibraev <ramil_ibraev@epam.com>
2019-03-05 14:28:16 +0300

    book service

---
+++ book-service/src/main/java/com/demo/book/BookServiceInitializer.java
@@ -0,0 +1,9 @@
+package com.demo.book;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BookServiceInitializer {
+}
